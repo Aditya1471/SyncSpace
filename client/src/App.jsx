bed3c356c81c396e31c16d305054180bafac3021
satyam kumar <xsatyam10@gmail.com>
2026-07-14 17:59:56 +0530

    established connection between client and server using socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from "react";
-import CodeEditor from "./components/CodeEditor";
-
-function App() {
-  const [code, setCode] = useState("// Start coding...");
-
-  return (
-    <CodeEditor
-      language="javascript"
-      value={code}
-      onChange={setCode}
-    />
-  );
-}
-
-export default App;
+import { useState, useEffect } from "react";
+     import { io } from "socket.io-client"; // Import socket.io
+    import CodeEditor from "./components/CodeEditor";
+    
+     // Connect to the server running on port 5000
+     const socket = io("http://localhost:5000");
+    
+     function App() {
+       const [code, setCode] = useState("// Start coding...");
+   
+      useEffect(() => {
+        // Join a default room so we can sync with others
+        socket.emit("join-room", { roomId: "default-room", username: "User" });
+   
+       // Listen for code changes from the server
+        socket.on("code-change", (newCode) => {
+          setCode(newCode);
+        });
+   
+       return () => socket.off("code-change");
+      }, []);
+   
+      const handleCodeChange = (newCode) => {
+       setCode(newCode);
+       // Send the change to the server to broadcast to other users
+       socket.emit("code-change", { roomId: "default-room", code: newCode });
+      };
+   
+      return (
+        <CodeEditor
+          language="javascript"
+         value={code}
+          onChange={handleCodeChange}
+        />
+     );
+   }
